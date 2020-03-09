5812a991da3bff9eabcaedd42e7bba65e22ae833
TommasoAzz <azzalintommaso@gmail.com>
2020-03-09 14:48:29 +0100

    Modificato lo script di compilazione

--- compile_all_latex.bat
+++ compile_all_latex.bat
@@ -1,4 +1,4 @@
-echo "Compilazione Documentazione Esterna"
+echo Compilazione Documentazione Esterna
 cd DocumentazioneEsterna
 
 cd AnalisiDeiRequisiti
@@ -23,7 +23,7 @@ cd ..\VE_2020_02_27
 latexmk -pdf VE_2020_02_27.tex > NUL
 latexmk -c > NUL
 
-echo "Compilazione Documentazione Interna"
+echo Compilazione Documentazione Interna
 cd ..\..\..\DocumentazioneInterna
 
 cd NormeDiProgetto
@@ -51,7 +51,7 @@ cd ..\VI_2019_12_10
 latexmk -pdf VI_2019_12_10.tex > NUL
 latexmk -c > NUL
 cd ..\VI_2019_12_13
-latexmk -pdf VI_2019_12_10.tex > NUL
+latexmk -pdf VI_2019_12_13.tex > NUL
 latexmk -c > NUL
 cd ..\VI_2019_12_18
 latexmk -pdf VI_2019_12_18.tex > NUL
@@ -59,11 +59,17 @@ latexmk -c > NUL
 cd ..\VI_2020_01_29
 latexmk -pdf VI_2020_01_29.tex > NUL
 latexmk -c > NUL
+cd ..\VI_2020_02_15
+latexmk -pdf VI_2020_02_15.tex > NUL
+latexmk -c > NUL
 cd ..\VI_2020_02_21
 latexmk -pdf VI_2020_02_21.tex > NUL
 latexmk -c > NUL
 cd ..\VI_2020_02_24
 latexmk -pdf VI_2020_02_24.tex > NUL
+latexmk -c > NUL
+cd ..\VI_2020_02_26
+latexmk -pdf VI_2020_02_26.tex > NUL
 latexmk -c > NUL
 
 cd ..\..\..
@@ -71,5 +77,69 @@ cd ..\..\..
 cd Glossario
 latexmk -pdf Glossario.tex > NUL
 latexmk -c > NUL
+
+cd ..
+
+echo Copia dei file in un unica cartella
+
+
+echo Compilazione Documentazione Esterna
+cd DocumentazioneEsterna
+
+cd AnalisiDeiRequisiti
+copy AnalisiDeiRequisiti.pdf ..\..\FileCompilati\.
+cd ..\PianoDiProgetto
+copy PianoDiProgetto.pdf ..\..\FileCompilati\.
+cd ..\PianoDiQualifica
+copy PianoDiQualifica.pdf ..\..\FileCompilati\.
+
+cd ..\VerbaliEsterni
+
+cd VE_2019_12_16
+copy VE_2019_12_16.pdf ..\..\..\FileCompilati\.
+cd ..\VE_2020_02_18
+copy VE_2020_02_18.pdf ..\..\..\FileCompilati\.
+cd ..\VE_2020_02_27
+copy VE_2020_02_27.pdf ..\..\..\FileCompilati\.
+
+echo Compilazione Documentazione Interna
+cd ..\..\..\DocumentazioneInterna
+
+cd NormeDiProgetto
+copy NormeDiProgetto.pdf ..\..\FileCompilati\.
+cd ..\StudioDiFattibilita
+copy StudioDiFattibilita.pdf ..\..\FileCompilati\.
+
+cd ..\VerbaliInterni
+
+cd VI_2019_11_20
+copy VI_2019_11_20.pdf ..\..\..\FileCompilati\.
+cd ..\VI_2019_11_27
+copy VI_2019_11_27.pdf ..\..\..\FileCompilati\.
+cd ..\VI_2019_12_03\
+copy VI_2019_12_03.pdf ..\..\..\FileCompilati\.
+cd ..\VI_2019_12_06
+copy VI_2019_12_06.pdf ..\..\..\FileCompilati\.
+cd ..\VI_2019_12_10
+copy VI_2019_12_10.pdf ..\..\..\FileCompilati\.
+cd ..\VI_2019_12_13
+copy VI_2019_12_10.pdf ..\..\..\FileCompilati\.
+cd ..\VI_2019_12_13
+copy VI_2019_12_13.pdf ..\..\..\FileCompilati\.
+cd ..\VI_2020_01_29
+copy VI_2020_01_29.pdf ..\..\..\FileCompilati\.
+cd ..\VI_2020_02_15
+copy VI_2020_02_15.pdf ..\..\..\FileCompilati\.
+cd ..\VI_2020_02_21
+copy VI_2020_02_21.pdf ..\..\..\FileCompilati\.
+cd ..\VI_2020_02_24
+copy VI_2020_02_24.pdf ..\..\..\FileCompilati\.
+cd ..\VI_2020_02_26
+copy VI_2020_02_26.pdf ..\..\..\FileCompilati\.
+
+cd ..\..\..
+
+cd Glossario
+copy Glossario.pdf ..\FileCompilati\.
 
 cd ..
